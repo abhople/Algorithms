10f46057f2b8246371e0597b40fd9d1fab8727f2
Anupam Bhople <anupam.bhople@gmail.com>
2019-08-19 20:59:12 -0400

    Create Main.java

---
+++ BinarySearchTree/Main.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package com.test.binarytree;
+
+/**
+ * @author Anupam Bhople
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		Tree binTree = new Tree();
+		binTree.insert(10);
+		binTree.insert(12);
+		binTree.insert(88);
+		binTree.insert(-9);
+		binTree.insert(34);
+		binTree.insert(2);
+		binTree.insert(66);
+		binTree.insert(72);
+		binTree.insert(14);
+		
+		binTree.levelOrderTraversal();
+		
+	}
+
+}
